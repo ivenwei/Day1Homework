36d76b4d0ee01a2a844ad49efc16a04831932ef8
ivenwei <ivenwei@gmail.com>
2016-08-05 20:57:47 +0800

    First Commit

---
+++ HomeWork1Day1/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(HomeWork1Day1.Startup))]
+namespace HomeWork1Day1
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
---
+++ HomeworkDay1/HomeworkDay1/Models/ViewModels/MyAccountViewModels.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Web;
+
+namespace HomeworkDay1.Models.ViewModels
+{
+    public class MyAccountViewModels
+    {
+
+        [DisplayName("類別")]
+        public string category { get; set; }
+
+        [DisplayName("金額")]
+        public decimal myMoney { get; set; }
+
+        [DisplayName("日期")]
+        public DateTime date { get; set; }
+
+        [DisplayName("備註")]
+        public string memo { get; set; }
+    }
+}
